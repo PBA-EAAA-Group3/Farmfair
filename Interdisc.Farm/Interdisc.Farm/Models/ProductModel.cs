2fd7bd52d23669ce4648f77b44c7447c528b6bd2
sholm1 <74183507+sholm1@users.noreply.github.com>
2020-11-30 15:02:22 +0100

    DatabaseFirstTest

--- Interdisc.Farm/Interdisc.Farm/Models/ProductModel.cs
+++ Interdisc.Farm/Interdisc.Farm/Models/ProductModel.cs
@@ -8,25 +8,50 @@ namespace Interdisc.Farm.Models
     public class ProductModel
     {
         public int ProductID { get; set; }
-        public int ProductGroupID { get; set; }
+        //public int ProductGroupID { get; set; }
         public int Batch { get; set; }
         public string ProductName{ get; set; }
-        public int FarmID { get; set; }
+        //public int FarmID { get; set; }
 
         public decimal Price { get; set; }
-
-
-        public ProductModel () { }
-        public ProductModel(int productid, int productgroupid, int batch, string productname, int farmid, decimal price)
-        {
-            ProductID = productid;
-            ProductGroupID = productgroupid;
-            Batch = batch;
-            ProductName = productname;
-            FarmID = farmid;
-            Price = price;
-
-        }
-
     }
+
+    public class ProductGroupModel
+    {
+        public int ProductGroupID { get; set; }
+        public string ProductGroupName { get; set; }
+    }
+    
+    public class  FarmModel 
+    {
+        public int FarmID { get; set; }
+        public string FarmName { get; set; }
+        //public int ProductID { get; set; }
+        //public int ProductGroupID { get; set; }
+    }
+
+    public class InvoiceLineModel
+    {
+        public int InvoiceLineID { get; set; }
+        //public int InvoiceID { get; set; }
+        //public int ProductID { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal SumPrice { get; set; }
+    }
+    public class InvoiceModel
+    {
+        public int InvoiceID { get; set; }
+        //public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalSum { get; set; }
+    }
+    public class CustomerModel 
+    {
+        public int CustomerID { get; set; }
+        public int CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+    public class FarmDbContext : DbContext 
 }
